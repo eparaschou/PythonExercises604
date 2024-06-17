2f282c7b03858771cee36d6bdc041881f2faacea
Paschalina Paraschou <pp3639a@american.edu>
2024-06-17 18:42:12 -0400

    Commit

---
+++ Modules.py
@@ -0,0 +1,14 @@
+import math
+
+# area of circle
+r = 10
+area = math.pi * (r**2)
+print("Area of the circle is:", area)
+
+sqrrt_3 = math.sqrt(3)
+print("Square root of 3 is:",sqrrt_3)
+
+import calendar
+print("Is 2019 a leap year?",calendar.isleap(2019))
+print("Weekday:", calendar.weekday(2020,10,11))
+
